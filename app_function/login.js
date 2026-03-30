a603f0b1e10cabc6a70a7ef31df898519c11e377
TKR-503 <tawhidrafe2050@gmail.com>
2026-03-30 21:24:42 +0600

    Rename some folder

--- app_function/login.js
+++ app_function/login.js
@@ -6,12 +6,14 @@ document.getElementById("login-btn").addEventListener("click",  function(e){
     const user_input = document.getElementById("user").value;
     const password_input = document.getElementById("password").value;
 
-   if(user_input === "admin" && password_input === "admin123") {
+   if(user_input === "admin" && password_input === "admin123") 
+    {
         
         alert("Login successful! Welcome to the GitHub Issues Tracker.");
 
-        window.location.assign("/home_page.html");
-   } else 
+        window.location.assign("/dashboard.html");
+   } 
+   else 
         {
            alert("Invalid username or password.Please try again.");
           return;
